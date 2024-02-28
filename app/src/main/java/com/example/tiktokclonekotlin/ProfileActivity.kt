5f810c66aea40e8b8d83cabebd2fc00591604ab7
tonywarwick69 <duc.khang165@gmail.com>
2024-02-28 08:49:29 +0700

    Add upload profile picture to Firebase storage

--- app/src/main/java/com/example/tiktokclonekotlin/ProfileActivity.kt
+++ app/src/main/java/com/example/tiktokclonekotlin/ProfileActivity.kt
@@ -1,10 +1,18 @@
 package com.example.tiktokclonekotlin
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -18,6 +26,8 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
+import com.google.firebase.storage.FirebaseStorage
+import java.net.URI
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -25,6 +35,8 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
     lateinit var currentUserID : String
     lateinit var profileUserModel: UserModel
+    lateinit var photoLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,10 +44,21 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
         profileUserID = intent.getStringExtra("profile_user_id")!!
         currentUserID = FirebaseAuth.getInstance().currentUser?.uid!!
+        //Init photolauncher activity
+        photoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+            if(result.resultCode == RESULT_OK){
+                //upload photo
+                uploadPhotoToFirestore(result.data?.data!!)
+            }
+        }
+
         if(profileUserID == currentUserID){
             binding.btnLogOut.text= "Logout"
             binding.btnLogOut.setOnClickListener {
                 logOut()
+            }
+            binding.profilePic.setOnClickListener{
+                checkPermissionAndPickPhoto()
             }
         } else {
             //other user profile
@@ -44,7 +67,49 @@ class ProfileActivity : AppCompatActivity() {
 
 
     }
-
+    fun uploadPhotoToFirestore(photoURI: Uri){
+            binding.progressBar.visibility = View.VISIBLE
+            val photoRef = FirebaseStorage.getInstance().reference
+                .child("profilePic/"+ currentUserID)
+            photoRef.putFile(photoURI)
+                .addOnSuccessListener {
+                    photoRef.downloadUrl.addOnSuccessListener {
+                            downloadUrl ->
+                        //video model store in firebase firestore
+                        postToFirestore(downloadUrl.toString())
+                    }
+                }
+    }
+    fun postToFirestore(url: String){
+        Firebase.firestore.collection("users")
+            .document(currentUserID)
+            .update("profilePic",url)
+            .addOnSuccessListener {
+                setUI()
+            }
+    }
+    private fun checkPermissionAndPickPhoto(){
+        var readExternalPhoto : String = ""
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            readExternalPhoto= android.Manifest.permission.READ_MEDIA_IMAGES
+        } else{
+            readExternalPhoto= android.Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+        if (ContextCompat.checkSelfPermission(this,readExternalPhoto)== PackageManager.PERMISSION_GRANTED){
+            openPhotoPicker()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(readExternalPhoto),
+                100
+            )
+        }
+    }
+    private fun openPhotoPicker(){
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+        intent.type= "image/*"
+        photoLauncher.launch(intent)
+    }
     private fun logOut() {
         FirebaseAuth.getInstance().signOut()
         val intent = Intent(this,LoginActivity::class.java)
@@ -68,6 +133,7 @@ class ProfileActivity : AppCompatActivity() {
             Glide.with(binding.profilePic)
                 .load(profilePic)
                 .apply { RequestOptions().placeholder(R.drawable.account_circle_icon) }
+                .circleCrop()
                 .into(binding.profilePic)
             binding.txtProfileUsername.text="@"+username
             binding.progressBar.visibility= View.INVISIBLE
